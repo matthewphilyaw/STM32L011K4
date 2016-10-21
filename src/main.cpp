d7f5977afbd7fa3b45b03d494788cfda1af5b3f9
Matthew Philyaw <matthew.philyaw@gmail.com>
2016-10-21 00:02:38 -0400

    STM32L0 base

---
+++ src/main.cpp
@@ -0,0 +1,97 @@
+#include "main.h"
+
+static GPIO_InitTypeDef GPIO_InitStruct;
+
+static void SystemClock_Config(void);
+
+int main(void)
+{
+  HAL_Init();
+
+  /* Configure the system clock to 2 MHz */
+  SystemClock_Config();
+
+  /* -1- Enable GPIO Clock (to be able to program the configuration registers) */
+  LED3_GPIO_CLK_ENABLE();
+
+  /* -2- Configure IO in output push-pull mode to drive external LEDs */
+  GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull  = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+
+  GPIO_InitStruct.Pin = LED3_PIN;
+  HAL_GPIO_Init(LED3_GPIO_PORT, &GPIO_InitStruct);
+
+  /* -3- Toggle IO in an infinite loop */
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LED3_GPIO_PORT, LED3_PIN);
+    /* Insert delay 100 ms */
+    HAL_Delay(500);
+  }
+  for (;;) {
+
+  }
+}
+
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  
+  /* Enable MSI Oscillator */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_5;
+  RCC_OscInitStruct.MSICalibrationValue=0x00;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+  
+  /* Select MSI as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  
+  /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  
+  /* Disable Power Control clock */
+  __HAL_RCC_PWR_CLK_DISABLE();
+  
+}
+
+
+void reset_handler() {
+
+}
+
+void _init() {
+
+}
+
+#ifdef  USE_FULL_ASSERT
+
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  while (1)
+  {
+  }
+}
+
+#endif
---
+++ system/include/cmsis/cmsis_device.h
@@ -0,0 +1,6 @@
+#ifndef __CMSIS_DEVICE_H__
+#define __CMSIS_DEVICE_H__
+
+#include "stm32L0xx.h"
+
+#endif /* ifndef CMSIS_DEVICE_H */
